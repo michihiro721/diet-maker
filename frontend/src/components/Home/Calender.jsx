9496e48c1bc09f1c826ebcc63081aa4d860424fe
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-18 04:43:10 +0900

    カレンダーのデザインを整えた

--- frontend/src/components/Home/Calender.jsx
+++ frontend/src/components/Home/Calender.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import React, { useState } from 'react'; // reactとuseStateをインポート
+import Calendar from 'react-calendar'; // react-calendarをインポート
+import 'react-calendar/dist/Calendar.css'; // カレンダーのCSSファイルをインポート
+import './Calender.css'; // カスタムCSSファイルをインポート
 
 function Calender() {
   const [date, setDate] = useState(new Date());
