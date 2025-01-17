552a31bc9001c81ad356b9277b317a9115aacffc
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-18 04:56:47 +0900

    機能についてのコメントを追加

--- frontend/src/components/Home/Calender.jsx
+++ frontend/src/components/Home/Calender.jsx
@@ -1,17 +1,20 @@
-import React, { useState } from 'react'; // reactとuseStateをインポート
-import Calendar from 'react-calendar'; // react-calendarをインポート
-import 'react-calendar/dist/Calendar.css'; // カレンダーのCSSファイルをインポート
+import React, { useState } from 'react'; // ReactとuseStateフックをインポート
+import Calendar from 'react-calendar'; // react-calendarコンポーネントをインポート
+import 'react-calendar/dist/Calendar.css'; // カレンダーのデフォルトCSSファイルをインポート
 import './Calender.css'; // カスタムCSSファイルをインポート
 
 function Calender() {
+  // 日付の状態を管理するためのuseStateフックを使用
   const [date, setDate] = useState(new Date());
 
+  // カレンダーの日付が変更されたときに呼び出される関数
   const onChange = (newDate) => {
-    setDate(newDate);
+    setDate(newDate); // 状態を新しい日付に更新
   };
 
   return (
     <div>
+      {/* カレンダーコンポーネントを表示し、onChangeとvalueプロパティを設定 */}
       <Calendar
         onChange={onChange}
         value={date}
@@ -20,4 +23,4 @@ function Calender() {
   );
 }
 
-export default Calender;
+export default Calender; // Calenderコンポーネントをエクスポート
