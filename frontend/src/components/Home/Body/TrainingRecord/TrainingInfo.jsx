ddca9d0453979aa89e262d262e694947e4882ea8
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-19 04:20:51 +0900

    トレーニング記録の箇所の見た目を整えた

--- frontend/src/components/Home/Body/TrainingRecord/TrainingInfo.jsx
+++ frontend/src/components/Home/Body/TrainingRecord/TrainingInfo.jsx
@@ -4,7 +4,7 @@ import './styles/training-info.css';
 const TrainingInfo = () => (
   <div className="training-info">
     <p>
-      種目：<input type="text" defaultValue="ベンチプレス" readOnly />
+      種目：<input type="text" className="exercise-input" defaultValue="ベンチプレス" readOnly />
     </p>
     <p>対象部位：胸</p>
     <p>MAX重量：95kg</p>
