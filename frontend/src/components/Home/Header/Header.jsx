80017d6f005453bbf73271ce5e1ec71d0169e481
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-20 19:49:09 +0900

    ヘッダー右上にボタンを配置して、モーダルウィンドウ追加

--- frontend/src/components/Home/Header/Header.jsx
+++ frontend/src/components/Home/Header/Header.jsx
@@ -1,19 +1,55 @@
 // このコードは、ウェブサイトのヘッダーを表示するためのコンポーネントです。
 // ヘッダーには、サイトのタイトルが含まれています。
 
-import React from 'react';
-import './Header.css'; // CSSファイルをインポート
+import React, { useState } from "react";
+import "./styles/header.css"; // CSSファイルをインポート
 
 // Headerコンポーネントを定義
-function Header() {
-    return (
-        // ヘッダー要素を返す。クラス名はheaderを使用
-        <header className="header">
-            {/* タイトル要素を返す。クラス名はtitleを使用 */}
-            <h1 className="title">ダイエットメーカー</h1>
-        </header>
-    );
-}
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <div>
+      {/* ヘッダー */}
+      <header className="header">
+        <div className="header-title">ダイエットメーカー</div>
+        <button className="menu-button" onClick={toggleMenu}>
+          ≡
+        </button>
+      </header>
+
+      {/* モーダルウィンドウ */}
+      {isMenuOpen && (
+        <div className="menu-overlay" onClick={closeMenu}>
+          <div className="menu-modal" onClick={(e) => e.stopPropagation()}>
+            <ul className="menu-list">
+              <li>成果</li>
+              <li>目標設定</li>
+              <li>トレーニングメニュー提案</li>
+              <li>身体情報</li>
+              <li>カロリー関係</li>
+              <li>体重</li>
+              <li>ダイエット心構え</li>
+              <li>みんなの投稿一覧</li>
+              <li>アプリ使い方</li>
+            </ul>
+            <button className="close-button" onClick={closeMenu}>
+              閉じる
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 // Headerコンポーネントをエクスポート
 export default Header;
