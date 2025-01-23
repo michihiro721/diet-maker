e91933bedb6718a17909054ba3fc5892423c06e5
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-24 01:54:41 +0900

    Update render-build.sh to build frontend assets

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,14 @@ set -o errexit
 cd backend
 
 bundle install
+
+# Build JavaScript assets
+cd ../frontend
+bun install
+bun run build
+cd ../backend
+
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+bundle exec rails db:create
 bundle exec rake db:migrate
