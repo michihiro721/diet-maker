5c0b67a878671d76cbc65ef418b9963f18b4b955
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-24 02:59:34 +0900

    Ensure necessary files exist before installing jsbundling-rails

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,8 +6,13 @@ cd backend
 
 bundle install
 
-# Install jsbundling-rails
-bundle exec rails javascript:install:esbuild
+# Ensure necessary files exist
+if [ ! -f "config/boot.rb" ]; then
+  echo "config/boot.rb not found, skipping jsbundling-rails installation"
+else
+  # Install jsbundling-rails
+  bundle exec rails javascript:install:esbuild
+fi
 
 # Build JavaScript assets
 cd ../frontend
