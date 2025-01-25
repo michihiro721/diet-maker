5b7be008fcf884a00b5b7d236b73086d2f293b7f
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-26 04:19:34 +0900

    Fix Bundler version installation in render-build.sh

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -5,6 +5,10 @@ set -o errexit
 # Change to the backend directory
 cd backend
 
+# Install the specified version of Bundler
+gem install bundler -v 2.6.2
+
+# Use the specified version of Bundler
 bundle _2.6.2_ install
 
 # Check if assets:precompile task exists
