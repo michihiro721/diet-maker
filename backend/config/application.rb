11aeca27b9de1cbd2c55f55d2506129bea75fbb7
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-29 10:24:09 +0900

    Dotenv::Railtie.load に変更

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Rails.load
+Dotenv::Railtie.load
 
 module YourAppName
   class Application < Rails::Application
