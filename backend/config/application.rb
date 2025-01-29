806887e9b2d87c5688074d80c4b072d0bb1d3de4
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-29 19:49:26 +0900

    Fix dotenv and ActiveRecord issues

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -6,9 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-if defined?(Dotenv)
-  Dotenv::Railtie.load
-end
+Dotenv::Rails.load
 
 module YourAppName
   class Application < Rails::Application
