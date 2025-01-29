34f3980430a974f290dcc08d9f1fd7a49f0039fc
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-29 19:10:56 +0900

    元に戻した

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require 'dotenv/rails-now' if Rails.env.development? || Rails.env.test?
+Dotenv::Railtie.load
 
 module YourAppName
   class Application < Rails::Application
