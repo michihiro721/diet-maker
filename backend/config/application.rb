8c01989abb5101bec758f3ecad49a6cd0956034b
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-29 10:48:12 +0900

    Fix dotenv initialization and add puma configuration

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Rails.load
+require 'dotenv/rails-now'
 
 module YourAppName
   class Application < Rails::Application
