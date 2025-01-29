f266f43125ffead8c4fc71e0eea44bf4c286283a
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-29 19:46:17 +0900

    Move dotenv-rails out of development/test group

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -6,7 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+if defined?(Dotenv)
+  Dotenv::Railtie.load
+end
 
 module YourAppName
   class Application < Rails::Application
