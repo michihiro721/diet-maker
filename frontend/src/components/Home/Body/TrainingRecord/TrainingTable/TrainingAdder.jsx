12ee27bf174de0f9fac599693c0f17b6394cdb89
michihiro721 <ms.michihiro.0721@gmail.com>
2025-02-15 16:13:44 +0900

    トレーニング追加ボタンのデザインを整えた

--- frontend/src/components/Home/Body/TrainingRecord/TrainingTable/TrainingAdder.jsx
+++ frontend/src/components/Home/Body/TrainingRecord/TrainingTable/TrainingAdder.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import './styles/training-adder.css';
 
 const TrainingAdder = ({ addTraining }) => {
-  return (
+return (
     <div className="training-adder">
-      <button onClick={addTraining}>トレーニング追加</button>
+        <button className="add-training-button" onClick={addTraining}>トレーニング追加</button>
     </div>
-  );
+);
 };
 
 export default TrainingAdder;
