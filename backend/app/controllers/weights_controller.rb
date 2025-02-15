bc2312bca92bd3115282be44caf63e1e751e27c1
michihiro721 <ms.michihiro.0721@gmail.com>
2025-02-15 11:50:52 +0900

    保存ボタンdb反映

--- backend/app/controllers/weights_controller.rb
+++ backend/app/controllers/weights_controller.rb
@@ -16,6 +16,6 @@ class WeightsController < ApplicationController
   private
 
   def weight_params
-    params.require(:weight).permit(:user_id, :date, :weight)
+    params.require(:weight).permit(:date, :weight)
   end
 end
