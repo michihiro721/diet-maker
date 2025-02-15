da71729f678bd21ee54152fc1a04a17bd5715956
michihiro721 <ms.michihiro.0721@gmail.com>
2025-02-15 18:57:05 +0900

    トレーニングメニューを追加した際に、タイマーの時間がデフォルトで2:00になるように設定

--- frontend/src/components/Home/Body/TrainingRecord/TrainingTable/TrainingAdder.jsx
+++ frontend/src/components/Home/Body/TrainingRecord/TrainingTable/TrainingAdder.jsx
@@ -15,9 +15,24 @@ const TrainingAdder = ({ addTraining, deleteTraining }) => {
     setDeleteModalVisible(false);
   };
 
+  const handleAddTraining = () => {
+    const newTraining = {
+      exercise: "",
+      targetArea: "",
+      maxWeight: 0,
+      calories: 0,
+      sets: [
+        { weight: 0, reps: 0, complete: false, timer: "02:00" },
+        { weight: 0, reps: 0, complete: false, timer: "02:00" },
+        { weight: 0, reps: 0, complete: false, timer: "02:00" },
+      ],
+    };
+    addTraining(newTraining);
+  };
+
   return (
     <div className="training-adder">
-      <button className="add-training-button" onClick={addTraining}>トレーニング追加</button>
+      <button className="add-training-button" onClick={handleAddTraining}>トレーニング追加</button>
       {deleteModalVisible && (
         <div className="delete-modal">
           <div className="delete-modal-content">
