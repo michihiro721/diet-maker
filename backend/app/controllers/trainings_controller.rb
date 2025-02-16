5941c60b2f81e025bf97ff0cb4144c5e4f444d37
michihiro721 <ms.michihiro.0721@gmail.com>
2025-02-16 15:45:45 +0900

    トレーニングdb

--- backend/app/controllers/trainings_controller.rb
+++ backend/app/controllers/trainings_controller.rb
@@ -1,17 +1,28 @@
 class TrainingsController < ApplicationController
   def create
-    training = Training.new(training_params)
+    training_params[:sets].each do |set|
+      training = Training.new(
+        user_id: training_params[:user_id],
+        goal_id: training_params[:goal_id],
+        workout_id: training_params[:workout_id],
+        date: training_params[:date],
+        sets: set[:sets],
+        reps: set[:reps],
+        weight: set[:weight]
+      )
 
-    if training.save
-      render json: training, status: :created
-    else
-      render json: training.errors, status: :unprocessable_entity
+      unless training.save
+        render json: training.errors, status: :unprocessable_entity
+        return
+      end
     end
+
+    render json: { message: 'Training records saved successfully' }, status: :created
   end
 
   private
 
   def training_params
-    params.require(:training).permit(:date, :user_id, sets: [:weight, :reps, :complete, :timer])
+    params.require(:training).permit(:date, :user_id, :goal_id, :workout_id, sets: [:sets, :reps, :weight])
   end
 end
