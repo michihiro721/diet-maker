867c0c872bda6ec7ba44a447ffe9cad40e81b7c8
michihiro721 <ms.michihiro.0721@gmail.com>
2025-02-19 15:30:26 +0900

    db

--- frontend/src/components/Home/Body/TrainingRecord/TrainingTable/TrainingRecord.jsx
+++ frontend/src/components/Home/Body/TrainingRecord/TrainingTable/TrainingRecord.jsx
@@ -176,7 +176,9 @@ const TrainingRecord = () => {
 
     try {
       // 新しいデータを保存
-      const response = await axios.post('https://diet-maker-d07eb3099e56.herokuapp.com/trainings', { trainings: trainingData });
+      const response = await axios.post('https://diet-maker-d07eb3099e56.herokuapp.com/trainings', {
+        trainings: trainingData
+      });
 
       if (response.status !== 201) {
         throw new Error('Training data could not be saved');
