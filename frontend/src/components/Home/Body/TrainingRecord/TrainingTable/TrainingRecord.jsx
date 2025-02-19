7a5770dd39f81ad05fa2ed1645bd4ecece46f5cb
michihiro721 <ms.michihiro.0721@gmail.com>
2025-02-19 16:10:58 +0900

    セット追加ボタンをクリックした時のエラーを解決

--- frontend/src/components/Home/Body/TrainingRecord/TrainingTable/TrainingRecord.jsx
+++ frontend/src/components/Home/Body/TrainingRecord/TrainingTable/TrainingRecord.jsx
@@ -70,7 +70,7 @@ const TrainingRecord = () => {
     };
     const updatedTrainings = trainings.map((training, index) =>
       index === trainingIndex
-        ? { ...training, sets: [...training.sets, newSet] }
+        ? { ...training, sets: [...(training.sets || []), newSet] } // setsが未定義の場合は空の配列を設定
         : training
     );
     setTrainings(updatedTrainings);
@@ -176,9 +176,7 @@ const TrainingRecord = () => {
 
     try {
       // 新しいデータを保存
-      const response = await axios.post('https://diet-maker-d07eb3099e56.herokuapp.com/trainings', {
-        trainings: trainingData
-      });
+      const response = await axios.post('https://diet-maker-d07eb3099e56.herokuapp.com/trainings', { training: { trainings: trainingData } });
 
       if (response.status !== 201) {
         throw new Error('Training data could not be saved');
