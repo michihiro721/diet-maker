68de1627ec17fe55799c0cc57b170dd2dfaa2dac
michihiro721 <ms.michihiro.0721@gmail.com>
2025-02-19 15:18:51 +0900

    db

--- backend/app/controllers/trainings_controller.rb
+++ backend/app/controllers/trainings_controller.rb
@@ -1,26 +1,40 @@
 class TrainingsController < ApplicationController
-  def index
-    if params[:date]
-      date = Date.parse(params[:date])
-      trainings = Training.where(date: date)
-    else
-      trainings = Training.all
-    end
-    render json: trainings
-  end
-
   def create
-    training = Training.new(training_params)
-    if training.save
-      render json: training, status: :created
-    else
-      render json: training.errors, status: :unprocessable_entity
+    # 受け取ったトレーニングデータの日付とユーザーIDを取得
+    date = training_params[:trainings].first[:date]
+    user_id = training_params[:trainings].first[:user_id]
+
+    # 該当の日付とユーザーIDの既存データを取得
+    existing_trainings = Training.where(date: date, user_id: user_id)
+
+    # 既存データを削除
+    existing_trainings.destroy_all
+
+    # 新しいデータを保存
+    training_params[:trainings].each do |training|
+      new_training = Training.new(
+        user_id: training[:user_id],
+        goal_id: training[:goal_id],
+        workout_id: training[:workout_id],
+        date: training[:date],
+        sets: training[:sets],
+        reps: training[:reps],
+        weight: training[:weight]
+      )
+
+      unless new_training.save
+        Rails.logger.error new_training.errors.full_messages
+        render json: new_training.errors, status: :unprocessable_entity
+        return
+      end
     end
+
+    render json: { message: 'Training records saved successfully' }, status: :created
   end
 
   private
 
   def training_params
-    params.require(:training).permit(:user_id, :goal_id, :workout_id, :date, :sets, :reps, :weight)
+    params.permit(trainings: [:date, :user_id, :goal_id, :workout_id, :sets, :reps, :weight])
   end
 end
