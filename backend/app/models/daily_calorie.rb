c1f667e8e135cb1650d9ff8662fe591b6c2ebdb7
michihiro721 <ms.michihiro.0721@gmail.com>
2025-02-22 12:18:42 +0900

    faraday-retry追加

--- backend/app/models/daily_calorie.rb
+++ backend/app/models/daily_calorie.rb
@@ -2,7 +2,7 @@ class DailyCalorie < ApplicationRecord
   belongs_to :user
 
   validates :date, presence: true
-  validates :steps, presence: true
+
   validates :total_calories, presence: true
-  validates :intake_calories, presence: true
+
 end
