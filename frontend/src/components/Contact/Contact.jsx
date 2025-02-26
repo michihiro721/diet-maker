5eb5a7a2cc142ff65fa6bbf08f307a9bc9f5adfc
michihiro721 <ms.michihiro.0721@gmail.com>
2025-02-26 23:39:59 +0900

    お問合せ機能を実装（Googleフォームを埋め込んだ）

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -4,7 +4,16 @@ const Contact = () => {
   return (
     <div>
       <h1>お問い合わせ</h1>
-      <p>ここにお問い合わせに関する情報を表示します。</p>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfflAIxFFp2SSjWMUHgT8o6uLWU-e5USDqRGM2ohvFenegOTA/viewform?usp=header"
+        width="100%"
+        height="800"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+      >
+        読み込んでいます…
+      </iframe>
     </div>
   );
 };
