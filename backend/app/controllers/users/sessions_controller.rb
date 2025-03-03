4c55be7db1e6166467feb7d8064588c918b8c701
michihiro721 <ms.michihiro.0721@gmail.com>
2025-03-03 20:06:07 +0900

    ログイン機能のトークン

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -3,21 +3,26 @@ class Users::SessionsController < Devise::SessionsController
 
   private
 
+  # ログイン時のレスポンス
   def respond_with(resource, _opts = {})
-    render json: { message: 'Logged in successfully.' }, status: :ok
+    token = request.env['warden-jwt_auth.token']  # JWTトークンを取得
+    if token
+      headers['Authorization'] = "Bearer #{token}"  # 修正: ヘッダーを設定
+      render json: { message: 'Logged in successfully.', token: token }, status: :ok
+    else
+      render json: { message: 'JWT token is missing' }, status: :unauthorized
+    end
   end
 
+  # ログアウト時のレスポンス
   def respond_to_on_destroy
-    log_out_success && return if current_user
-
-    log_out_failure
-  end
-
-  def log_out_success
-    render json: { message: 'Logged out successfully.' }, status: :ok
-  end
-
-  def log_out_failure
-    render json: { message: "Logged out failure." }, status: :unauthorized
+    jwt_payload = request.env['warden-jwt_auth.token']
+    current_user&.update(jti: SecureRandom.uuid)  # ユーザーの JTI を変更（トークンの失効）
+    
+    if jwt_payload
+      render json: { message: 'Logged out successfully.' }, status: :ok
+    else
+      render json: { message: "Logged out failure." }, status: :unauthorized
+    end
   end
 end
