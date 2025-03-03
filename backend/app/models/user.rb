4536c9fad3a532a8abaf89f79943cf282ee7dda9
michihiro721 <ms.michihiro.0721@gmail.com>
2025-03-04 02:37:58 +0900

    元に戻した

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable,
+         :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   validates :email, presence: true, uniqueness: true
