80e73aa558051c019e2e56a4d249e6a20462f302
michihiro721 <ms.michihiro.0721@gmail.com>
2025-03-04 20:35:14 +0900

    修正

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -23,14 +23,17 @@ const Login = () => {
       });
 
 
-    if (res.status === 200) {
-      console.log(res.headers['authorization']);
-      // トークンをlocalStorageに保存
-      const token = res.headers['authorization'];
-      console.log("取得したトークン:", token);
-      if (token) {
-        localStorage.setItem('jwt', token.split(' ')[1]);
-      }
+      if (res.status === 200) {
+        console.log(res.headers);
+        
+        // `get` メソッドを試す
+        const token = res.headers.get('Authorization') || res.headers['authorization'];
+        
+        console.log("取得したトークン:", token);
+        
+        if (token) {
+            localStorage.setItem('jwt', token.split(' ')[1]);
+        }
     }
 
         alert('ログインに成功しました');
