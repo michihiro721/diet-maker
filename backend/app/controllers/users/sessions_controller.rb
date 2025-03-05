9b22c5f89762dc7cf2e78528bcce91ad6bdd6809
michihiro721 <ms.michihiro.0721@gmail.com>
2025-03-05 23:36:50 +0900

    プロフィール

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -3,26 +3,15 @@ class Users::SessionsController < Devise::SessionsController
 
   private
 
-  # ログイン時のレスポンス
   def respond_with(resource, _opts = {})
-    token = request.env['warden-jwt_auth.token']  # JWTトークンを取得
-    if token
-      response.set_header('Authorization', "Bearer #{token}")  # ヘッダーを設定
-      render json: { message: 'Logged in successfully.', token: token }, status: :ok
-    else
-      render json: { message: 'JWT token is missing' }, status: :unauthorized
-    end
+    render json: { user: resource, token: current_token }, status: :ok
   end
 
-  # ログアウト時のレスポンス
   def respond_to_on_destroy
-    jwt_payload = request.env['warden-jwt_auth.token']
-    current_user&.update(jti: SecureRandom.uuid)  # ユーザーの JTI を変更（トークンの失効）
-    
-    if jwt_payload
-      render json: { message: 'Logged out successfully.' }, status: :ok
-    else
-      render json: { message: "Logged out failure." }, status: :unauthorized
-    end
+    head :no_content
+  end
+
+  def current_token
+    request.env['warden-jwt_auth.token']
   end
 end
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -2,22 +2,20 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    user = User.find(params[:id])
-    render json: { email: user.email, name: user.name }
+    render json: { email: current_user.email, nickname: current_user.nickname }
   end
 
   def update
-    user = User.find(params[:id])
-    if user.update(user_params)
+    if current_user.update(user_params)
       render json: { message: 'Profile updated successfully.' }, status: :ok
     else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:nickname)
   end
 end
