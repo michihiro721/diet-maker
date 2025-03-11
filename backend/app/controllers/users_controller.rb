42fcc800b46c73f30f37e8ec8b400d8f251d39bc
michihiro721 <ms.michihiro.0721@gmail.com>
2025-03-12 03:44:17 +0900

    戻した

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,33 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :log_authentication_headers, only: [:show, :update]
 
   def show
-    user_id = params[:id].to_i
-    
-    # リクエストされたIDがログインユーザーと一致するか確認
-    if user_id == current_user.id
-      render json: {
-        email: current_user.email,
-        nickname: current_user.name,
-        weight: current_user.weight,
-        height: current_user.height,
-        age: current_user.age,
-        gender: current_user.gender
-      }
-    else
-      # 他ユーザー情報へのアクセスを試みている場合は基本情報のみ返す
-      # または完全に拒否することも可能
-      user = User.find_by(id: user_id)
-      if user
-        render json: {
-          nickname: user.name
-          # 公開情報のみ返すか、アクセス拒否にするかはポリシーによる
-        }
-      else
-        render json: { error: "User not found" }, status: :not_found
-      end
-    end
+    render json: {
+      email: current_user.email,
+      nickname: current_user.name,
+      weight: current_user.weight,
+      height: current_user.height,
+      age: current_user.age,
+      gender: current_user.gender
+    }
   end
 
   def update
@@ -52,11 +34,5 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :weight, :height, :age, :gender)
-  end
-  
-  def log_authentication_headers
-    Rails.logger.debug "Authorization Header: #{request.headers['Authorization']}"
-    Rails.logger.debug "Current User: #{current_user.inspect}" if user_signed_in?
-    Rails.logger.debug "Requested User ID: #{params[:id]}"
   end
 end
