7152e78dfe4ebf32bb5380a1926a0298f31ed6b8
michihiro721 <ms.michihiro.0721@gmail.com>
2025-03-16 23:18:29 +0900

    Googleログイン修正

--- backend/app/controllers/users/omniauth_callbacks_controller.rb
+++ backend/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,34 +1,23 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # CSRF保護を無効化
-  protect_from_forgery with: :null_session, only: [:google_oauth2]
+  # CSRF保護の設定
   skip_before_action :verify_authenticity_token, only: [:google_oauth2]
 
   def google_oauth2
     # デバッグログを追加
-    Rails.logger.info "Google OAuth callback received with auth: #{request.env['omniauth.auth']&.uid}"
+    Rails.logger.info "Google OAuth callback received"
     
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      # JWT形式のトークンを生成
-      token = JWT.encode(
-        { sub: @user.id, exp: 24.hours.from_now.to_i },
-        ENV['DEVISE_JWT_SECRET_KEY']
-      )
-      
-      # フロントエンドのURLへリダイレクト
-      redirect_url = "#{ENV['FRONTEND_URL'] || 'https://diet-maker-mu.vercel.app'}/auth/callback?token=#{token}"
-      Rails.logger.info "Redirecting to: #{redirect_url}"
-      redirect_to redirect_url
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
     else
-      # 登録失敗の場合
       session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
       redirect_to new_user_registration_url
     end
   end
 
   def failure
-    Rails.logger.error "Authentication failure: #{params[:message]}"
     redirect_to root_path
   end
 end
--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,3 +1,2 @@
 OmniAuth.config.allowed_request_methods = [:post, :get]
 OmniAuth.config.silence_get_warning = true
-OmniAuth.config.protect_from_forgery = false
