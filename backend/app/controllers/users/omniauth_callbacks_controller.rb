448592957e1363d21845b5581dec6a948a2864c1
michihiro721 <ms.michihiro.0721@gmail.com>
2025-03-16 18:51:50 +0900

    Googleログイン修正

--- backend/app/controllers/users/omniauth_callbacks_controller.rb
+++ backend/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # CSRF保護を無効化（APIモードでは必要）
-  skip_before_action :verify_authenticity_token
+
+  protect_from_forgery with: :null_session
 
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
@@ -13,14 +13,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       )
       
       # フロントエンドにリダイレクト（トークン付き）
-      redirect_to "#{ENV['FRONTEND_URL']}/auth/callback?token=#{token}"
+      redirect_to "#{ENV['FRONTEND_URL'] || 'http://localhost:8000'}/auth/callback?token=#{token}"
     else
       # 登録失敗の場合
-      redirect_to "#{ENV['FRONTEND_URL']}/login?error=auth_failed"
+      redirect_to "#{ENV['FRONTEND_URL'] || 'http://localhost:8000'}/login?error=auth_failed"
     end
   end
 
   def failure
-    redirect_to "#{ENV['FRONTEND_URL']}/login?error=#{params[:message]}"
+    redirect_to "#{ENV['FRONTEND_URL'] || 'http://localhost:8000'}/login?error=#{params[:message]}"
   end
 end
