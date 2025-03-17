876d72144615a93bd7a908f91ce90dbada0a5143
michihiro721 <ms.michihiro.0721@gmail.com>
2025-03-18 01:16:43 +0900

    Googleログイン機能実装修正7

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist,
+         :omniauthable
 
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
