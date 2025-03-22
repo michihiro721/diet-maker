2aa38defb46a4ea8357c59dda290bdafb0e29df0
michihiro721 <ms.michihiro.0721@gmail.com>
2025-03-23 02:53:25 +0900

    お問い合わせフォームの修正

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>お問い合わせ</h1>
+    <div style={{ marginTop: '80px', marginBottom: '80px' }}>
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSfflAIxFFp2SSjWMUHgT8o6uLWU-e5USDqRGM2ohvFenegOTA/viewform?usp=header"
         width="100%"
-        height="800"
+        height="1000"
         frameBorder="0"
         marginHeight="0"
         marginWidth="0"
