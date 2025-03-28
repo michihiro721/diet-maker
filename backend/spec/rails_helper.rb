aee7d3af6f4893cd08eead12bdebbdf71fff278e
michihiro721 <ms.michihiro.0721@gmail.com>
2025-03-29 06:25:47 +0900

    カバレッジ率を確認するコードを追加

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -1,3 +1,16 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/test/'
+  add_filter '/config/'
+  add_filter '/vendor/'
+  add_filter '/spec/'
+  
+  # モデルとコントローラーをグループ化
+  add_group 'Models', 'app/models'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Services', 'app/services'
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
