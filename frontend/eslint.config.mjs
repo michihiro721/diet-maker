cea930242901ac8a4f2dde62906efeb366e887be
michihiro721 <ms.michihiro.0721@gmail.com>
2025-04-03 12:19:34 +0900

    ESLintの導入

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { 
+      "react-hooks": reactHooksPlugin 
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn"
+    }
+  }
+];
