deef0803732e3924df07531bc3ff2f9b62482412
michihiro721 <ms.michihiro.0721@gmail.com>
2025-04-06 16:22:01 +0900

    ESLintチェック対応

--- frontend/src/components/Home/Body/TrainingRecord/CalculatorGrid/CalculatorGrid.jsx
+++ frontend/src/components/Home/Body/TrainingRecord/CalculatorGrid/CalculatorGrid.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import './styles/calculator.css';
+
 const CalculatorGrid = ({ handleCalculatorClick, handleBackspace }) => (
   <div className="calculator-grid">
-    {/* 数字ボタンを表示します */}
+    {/* 数字ボタンを表示 */}
     {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
       <button
         key={num}
@@ -12,11 +14,16 @@ const CalculatorGrid = ({ handleCalculatorClick, handleBackspace }) => (
         {num}
       </button>
     ))}
-    {/* 小数点ボタンを表示します */}
+    {/* 小数点ボタンを表示 */}
     <button className="calculator-button" onClick={() => handleCalculatorClick(".")}>.</button>
-    {/* バックスペースボタンを表示します */}
+    {/* バックスペースボタンを表示 */}
     <button className="calculator-button" onClick={handleBackspace}>&larr;</button>
   </div>
 );
+
+CalculatorGrid.propTypes = {
+  handleCalculatorClick: PropTypes.func.isRequired,
+  handleBackspace: PropTypes.func.isRequired
+};
 
 export default CalculatorGrid;
