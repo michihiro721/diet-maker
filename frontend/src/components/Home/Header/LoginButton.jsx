240e5bbf89691f43370e61123cd5e908a82af99c
michihiro721 <ms.michihiro.0721@gmail.com>
2025-04-07 12:39:02 +0900

    ESLintチェック対応 LoginButton.jsx

--- frontend/src/components/Home/Header/LoginButton.jsx
+++ frontend/src/components/Home/Header/LoginButton.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import LoginModal from "./LoginModal";
 import "./styles/LoginButton.css";
 
@@ -7,24 +6,19 @@ const LoginButton = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // ログイン状態をチェックする関数
+
   const checkLoginStatus = () => {
     const userId = localStorage.getItem('userId');
     setIsLoggedIn(userId !== null);
   };
 
-  // コンポーネントのマウント時およびローカルストレージの変更時にログイン状態を確認
   useEffect(() => {
-    // 初期チェック
     checkLoginStatus();
 
-    // ローカルストレージの変更を監視
     window.addEventListener('storage', checkLoginStatus);
 
-    // カスタムイベントをリッスン
     window.addEventListener('loginStateChanged', checkLoginStatus);
 
-    // クリーンアップ関数
     return () => {
       window.removeEventListener('storage', checkLoginStatus);
       window.removeEventListener('loginStateChanged', checkLoginStatus);
