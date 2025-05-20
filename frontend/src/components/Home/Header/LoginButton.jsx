6de6fd0f332f6a487e1dc058cf43cfa335af8c1c
michihiro721 <ms.michihiro.0721@gmail.com>
2025-05-21 08:10:44 +0900

    未ログインの状態だと、ログインのアイコンがヘッダーに表示されるように変更

--- frontend/src/components/Home/Header/LoginButton.jsx
+++ frontend/src/components/Home/Header/LoginButton.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import LoginModal from "./LoginModal";
+import { FaSignInAlt } from "react-icons/fa";
 import "./styles/LoginButton.css";
 
 const LoginButton = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
 
   const checkLoginStatus = () => {
     const userId = localStorage.getItem('userId');
@@ -14,11 +14,8 @@ const LoginButton = () => {
 
   useEffect(() => {
     checkLoginStatus();
-
     window.addEventListener('storage', checkLoginStatus);
-
     window.addEventListener('loginStateChanged', checkLoginStatus);
-
     return () => {
       window.removeEventListener('storage', checkLoginStatus);
       window.removeEventListener('loginStateChanged', checkLoginStatus);
@@ -39,7 +36,9 @@ const LoginButton = () => {
         className="header-login-button"
         onClick={toggleMenu}
       >
-        <i className={`fa-solid fa-user ${isLoggedIn ? 'user-logged-in' : ''}`}></i>
+        {isLoggedIn
+          ? <i className={`fa-solid fa-user user-logged-in`}></i>
+          : <FaSignInAlt className="fa-user" />}
       </button>
       {isMenuOpen && <LoginModal closeMenu={closeMenu} />}
     </div>
